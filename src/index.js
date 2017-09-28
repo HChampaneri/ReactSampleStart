130b01684371a74faf1e38fc7414f1f87f45a705
HEENA U. CHAMPANERI <Hchampaneri@galaxyps89.com>
2017-09-28 16:50:36 -0400

    checkin

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDOrQfO8reNVSe2pVFD3v-w78X53Ft_ge4';
+
 // const App = function() {
     
-const App = () => {
+YTSearch({key:API_KEY, term:'surfboards'},function(data){
+    console.log(data);
+})
+class App extends Component {
+    render() {
     return (
     <div>
         <SearchBar />
     </div>);
+    }
 }
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
